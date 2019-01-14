8656d6bff6f110f46e490e32a994012ee0e2083e
James Rosenfelt <james734@me.com>
2019-01-14 06:53:42 -0800

    Updated Copyright Year to 2019

--- DDT_Org_ChartModuleBase.cs
+++ DDT_Org_ChartModuleBase.cs
@@ -1,5 +1,5 @@
 /*
-' Copyright (c) 2010 DevPCI
+' Copyright (c) 2019 DevPCI
 '  All rights reserved.
 ' 
 ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
--- Providers/DataProviders/SqlDataProvider/SqlDataProvider.cs
+++ Providers/DataProviders/SqlDataProvider/SqlDataProvider.cs
@@ -1,5 +1,5 @@
 /*
-' Copyright (c) 2011 DevPCI
+' Copyright (c) 2019 DevPCI
 '  All rights reserved.
 ' 
 ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
